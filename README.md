2fd77f8bceb8cb7d2e1518db00e539470f3071c3
JustKato <potatomissing@gmail.com>
2022-11-03 10:09:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sveltejs-kit-template-default-v69b8g
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-v69b8g)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,87 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import { fade, blur, fly, slide, scale } from 'svelte/transition';
+	import { elasticOut } from 'svelte/easing';
+
+	function whoosh(node, params) {
+		const existingTransform = getComputedStyle(node).transform.replace('none', '');
+
+		return {
+			delay: params.delay || 0,
+			duration: params.duration || 400,
+			easing: params.easing || elasticOut,
+			css: (t, u) => `transform: ${existingTransform} scale(${t})`
+		};
+	}
+
+	let doShow = true;
+	let h = "";
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<source srcset={welcome} type="image/webp" />
+				<img src={welcome_fallback} alt="Welcome" />
+			</picture>
+		</span>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+	<Counter />
+
+	{#if doShow } 
+		<p 
+		on:outroend="{() => {h += "Ran\n"}}" out:slide in:whoosh>
+			TEST
+		</p>
+	{/if}
+
+	<button on:click={ () => { doShow = !doShow; }}>TESTT</button>
+	<br><br>
+	<pre>{h}</pre>
+
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
